b05e298042a4bc5ef3112584db21dcbbc46f6d93
Karabaye <niyoyitaisaiah@gmail.com>
2025-05-20 12:21:47 +0200

    newchange

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Pages from './Pages.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Pages/>
   </StrictMode>,
 )
