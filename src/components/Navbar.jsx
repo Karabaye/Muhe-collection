870e4cdca0cef4ea60aabe43ed8db9a02db8bc58
vibemutoni@gmail.com <vibemutoni@gmail.com>
2025-05-23 23:53:29 +0800

    every this i changed to day

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,12 @@
-import React, {useState} from 'react'
+import React, {useContext, useState} from 'react'
 import { assets } from '../assets/assets'
 import { Link, NavLink } from 'react-router-dom'
+import { ShopContext } from '../context/ShopContext'
 function Navbar() {
     const [visible, setVisible] = useState(false)
+
+    const {setShowSearch , getCartCount} = useContext(ShopContext);
+
   return (
     <div className='flex justify-between items-center py-4 font-medium'>
 
@@ -27,7 +31,7 @@ function Navbar() {
       </ul>
  
         <div className='flex item-center gap-5'>
-           <img src={assets.search_icon} className='w-5 cursor-pointer' alt="" /> 
+           <img onClick={()=>setShowSearch(true)} src={assets.search_icon} className='w-5 cursor-pointer' alt="" /> 
 
            <div className='group relative'>
              <img className='w-5 cursor-pointer' src={assets.profile_icon} alt="" />
@@ -41,7 +45,7 @@ function Navbar() {
            </div>
            <Link to='/cart' className='relative'>
               <img src={assets.cart_icon} className='w-5 min-w-5' alt="" />
-              <p className='absolute -top-2 -right-2 bg-red-500 text-white text-xs font-semibold px-1 rounded'>12</p>
+              <p className='absolute -top-2 -right-2 bg-red-500 text-white text-xs font-semibold px-1 rounded'>{getCartCount()}</p>
            </Link>
            <img onClick={() => setVisible(true)} src={assets.menu_icon} className='w-5 cursor-pointer sm:hidden' alt="" />
         </div>
--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,24 +1,107 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
+import { toast } from "react-toastify";
 
 export const ShopContext = createContext();
 
 const ShopContextProvider = (props) => {
-  
-  const currency = '$';
+  const currency = "$";
   const delivery_fee = 10;
+  const [search, setSearch] = useState("");
+  const [showSearch, setShowSearch] = useState(false);
+  const [cartItems, setCartItems] = useState({});
 
-const value = {
-    products,currency, delivery_fee
-}
+  const addToCart = async (itemId, size) => {
+    if (!size) {
+      toast.error("Select Product Size");
+      return;
+    }
 
-return (
-  <ShopContext.Provider value={value}>
-    {props.children}
-  </ShopContext.Provider>
-)
+    let cartData = structuredClone(cartItems);
 
+    if (cartData[itemId]) {
+      if (cartData[itemId][size]) {
+        cartData[itemId][size] += 1;
+      } else {
+        cartData[itemId][size] = 1;
+      }
+    } else {
+      cartData[itemId] = {};
+      cartData[itemId][size] = 1;
+    }
+    setCartItems(cartData);
+    toast.success("Item added to cart");
+  };
 
-}
+  const getCartCount = () => {
+    let totalCount = 0;
+    for (const itemId in cartItems) {
+      for (const size in cartItems[itemId]) {
+        const quantity = cartItems[itemId][size];
+        if (quantity > 0) {
+          totalCount += quantity;
+        }
+      }
+    }
+    return totalCount;
+  };
+
+  const updateQuantity = (itemId, size, quantity) => {
+    let cartData = structuredClone(cartItems);
+
+    if (quantity === 0) {
+      // Remove the size entry
+      delete cartData[itemId][size];
+
+      // If no sizes left for this item, remove the item completely
+      if (Object.keys(cartData[itemId]).length === 0) {
+        delete cartData[itemId];
+      }
+    } else {
+      if (!cartData[itemId]) {
+        cartData[itemId] = {};
+      }
+      cartData[itemId][size] = quantity;
+    }
+
+    setCartItems(cartData);
+  };
+
+  const getTotalCartAmount = () => {
+    let totalAmount = 0;
+    for (const itemId in cartItems) {
+      for (const size in cartItems[itemId]) {
+        const product = products.find((p) => p._id === itemId);
+        if (product) {
+          totalAmount += product.price * cartItems[itemId][size];
+        }
+      }
+    }
+    return totalAmount;
+  };
+
+  useEffect(() => {
+    console.log("Cart updated:", cartItems);
+  }, [cartItems]);
+
+  const value = {
+    products,
+    currency,
+    delivery_fee,
+    search,
+    setSearch,
+    showSearch,
+    setShowSearch,
+    cartItems,
+    addToCart,
+    getCartCount,
+    updateQuantity,
+    getTotalCartAmount,
+  };
+
+  return (
+    <ShopContext.Provider value={value}>{props.children}</ShopContext.Provider>
+  );
+};
 
 export default ShopContextProvider;
